76ee47f49386a2c1618ea0509787bb7a303440ae
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2020-09-20 13:19:36 -0500

    Clase Card

---
+++ Card.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Text;
+
+namespace clases
+{
+    class Card
+    {
+        string suit;
+        string symbol;
+        int score;
+        string color;
+        
+        public Card(string suit, string symbol)
+        {
+            this.suit = suit;
+            this.symbol = symbol;
+            if (suit == "♦" || suit == "♥")
+            {
+                color = "Red";
+            }
+            else
+            {
+                if (suit == "♣" || suit == "♠")
+                {
+                    color = "Black";
+                }
+            }
+            if (symbol == "A")
+            {
+                score = 1;
+            }
+            else
+            {
+                if (symbol == "J" || symbol == "Q" || symbol == "K")
+                {
+                    score = 10;
+                }
+                else
+                {
+                    int valueCard = Convert.ToInt32(symbol);
+                    score = valueCard;
+                }
+            }
+        }
+        public string Suit
+        {
+            get { return suit; }
+            set { suit = value; }
+        }
+
+        public string Symbol
+        {
+            get { return symbol; }
+            set { symbol = value; }
+        }
+
+        public string ShowIn()
+        {
+            return "Card: " + symbol + suit + " " + color ;
+        }
+    }
+}
